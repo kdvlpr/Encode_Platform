4d2f7df1e9a7cd6d5a35f0ce9ca3fc4baa92db20
Kian <f.kian.fallahi@gmail.com>
2017-05-22 12:18:58 +0430

    Update encode.py

--- Platform/encode.py
+++ Platform/encode.py
@@ -7,9 +7,9 @@ def en():
 
 	byte = text.encode('utf-8')
 
-	algorithm_compress =  base64.b64encode(zlib.compress(byte,9))
+	algorithm_encode =  base64.b64encode(zlib.compress(byte,9))
 
 
-	print ('\n'+str(algorithm_compress))
+	print ('\n'+str(algorithm_encode))
 
 	wait = input()
